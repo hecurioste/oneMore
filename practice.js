4bb00506e90fd8277c98bf7ab95e89e4da709e3d
hecurioste <hecurioste@gmail.com>
2022-03-27 19:49:50 -0500

    Add day

--- practice.js
+++ practice.js
@@ -1,5 +1,8 @@
 //This is a new file created just for practice
 
 const message = 'Hello World';
+let today = 1;
+let tommorrow = day + 1;
 
 console.log(message);
+console.log(`Today is day: ${today}, and tomorrow is ${tommorrow}`);
